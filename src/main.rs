faad0c957f7c9a276f77e8191c8be3a764d2d5f3
Kenner <lauren.miklancic@gmail.com>
2026-04-06 22:30:57 -0400

    getting boilerplate set up to open a window

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
+use std::sync::Arc;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::EventLoop;
 use winit::window::Window;
 
 struct App {
-    window: Option<Window>,
+    window: Option<Arc<Window>>,
+    surface: Option<wgpu::Surface<'static>>,
+    device: Option<wgpu::Device>,
+    queue: Option<wgpu::Queue>,
+    instance: Option<wgpu::Instance>,
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let window = event_loop
-            .create_window(Window::default_attributes())
-            .unwrap();
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+        self.instance = Some(instance);
         self.window = Some(window);
+        let surface = self
+            .instance
+            .as_ref()
+            .unwrap()
+            .create_surface(self.window.clone().unwrap())
+            .unwrap();
+        let adapter = pollster::block_on(
+            self.instance
+                .as_ref()
+                .unwrap()
+                .request_adapter(&wgpu::RequestAdapterOptions::default()),
+        )
+        .unwrap();
+        let (device, queue) =
+            pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor::default())).unwrap();
+        let size = self.window.as_ref().unwrap().inner_size();
+        let config = surface
+            .get_default_config(&adapter, size.width, size.height)
+            .unwrap();
+        surface.configure(&device, &config);
+        self.surface = Some(surface);
+        self.device = Some(device);
+        self.queue = Some(queue);
     }
     fn window_event(
         &mut self,
@@ -24,11 +56,18 @@ impl ApplicationHandler for App {
             self.window = None;
             event_loop.exit();
         }
+        if WindowEvent::RedrawRequested == event {}
     }
 }
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let mut app = App { window: None };
+    let mut app = App {
+        window: None,
+        surface: None,
+        device: None,
+        queue: None,
+        instance: None,
+    };
     let _ = event_loop.run_app(&mut app);
 }
